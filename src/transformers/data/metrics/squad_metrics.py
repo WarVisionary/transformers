575644c1dab63bb47cf7934823b616ef26060c9d
WarVisionary <omega.fa.fedorenko@gmail.com>
2020-06-08 23:45:35 +0300

    FIX: Parenthesis typo.

--- src/transformers/data/metrics/squad_metrics.py
+++ src/transformers/data/metrics/squad_metrics.py
@@ -563,7 +563,7 @@ def compute_predictions_logits(
             all_predictions[example.qas_id] = nbest_json[0]["text"]
         else:
             # predict "" iff the null score - the score of best non-null > threshold
-            score_diff = score_null - best_non_null_entry.start_logit - (best_non_null_entry.end_logit)
+            score_diff = score_null - (best_non_null_entry.start_logit + best_non_null_entry.end_logit)
             scores_diff_json[example.qas_id] = score_diff
             if score_diff > null_score_diff_threshold:
                 all_predictions[example.qas_id] = ""
